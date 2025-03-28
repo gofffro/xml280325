cea5da1a29671cc42f1271871a336c8106d9d986
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-03-28 21:04:07 +0700

    Добавлены свойства и принт метод

--- FileSer.cs
+++ FileSer.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace xml280325
 {
-  public class FileSer
+   public class FileSer
   {
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+
+    public FileSer() { }
+
+    public FileSer(string name, string surname, int age)
+    {
+      Name = name;
+      Surname = surname;
+      Age = age;
+    }
+
+    public void Print()
+    {
+      Console.WriteLine($"Name={Name} Surname={Surname} Age={Age}");
+    }
   }
 }

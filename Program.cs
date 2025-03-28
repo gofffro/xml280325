21a5d44cfa0fce3f5b17cc9ec3b0741501d0b7b8
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-03-28 22:04:37 +0700

    Создана базовая структура программы

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace xml280325
+﻿using System;
+
+namespace xml280325
 {
-    internal class Program
+  internal class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      string choice;
+
+      while (true)
+      {
+        Console.Write("Меню: \n" +
+            "0) Выход \n" +
+            "Ввод: ");
+        choice = Console.ReadLine();
+        Console.Clear();
+
+        if (choice == "0")
         {
-            Console.WriteLine("Hello, World!");
+          Console.WriteLine("Программа завершила работу");
+          break;
         }
+      }
     }
+  }
 }

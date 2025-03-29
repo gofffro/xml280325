afceba3b35d2c44a922dad2c20e825b4caba3033
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-03-29 11:32:20 +0700

    Переименнованы классы

---
+++ TextMemento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace xml280325
+{
+  public class TextMemento
+  {
+    public string State { get; private set; }
+
+    public TextMemento(string state)
+    {
+      State = state;
+    }
+  }
+
+  public class TextHistory
+  {
+    public TextMemento Memento { get; set; }
+  }
+
+  public class TextEditor
+  {
+    public string State { get; set; }
+
+    public void SetMemento(TextMemento memento)
+    {
+      State = memento.State;
+    }
+
+    public TextMemento CreateMemento()
+    {
+      return new TextMemento(State);
+    }
+  }
+}

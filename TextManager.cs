c553dd43f6762756e7eaf26b5ca263d07501af73
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-03-29 11:49:23 +0700

    Класс инкапсулирует взаимодействие между TextEditor и TextHistory

--- TextManager.cs
+++ TextManager.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace xml280325
 {
-  internal class TextManager
+  public class TextManager
   {
+    private readonly TextEditor _editor = new TextEditor();
+    private readonly TextHistory _history = new TextHistory();
+
+    public void SaveState()
+    {
+      _history.SaveMemento(_editor.CreateMemento());
+    }
+
+    public void RestoreState()
+    {
+      TextMemento memento = _history.GetMemento();
+      _editor.SetMemento(memento);
+    }
+
+    public string GetCurrentText()
+    {
+      return _editor.State;
+    }
+
+    public void UpdateText(string newText)
+    {
+      _editor.State = newText;
+    }
+
+    public void SaveToFile(string filePath)
+    {
+      File.WriteAllText(filePath, _editor.State);
+    }
+
+    public void LoadFromFile(string filePath)
+    {
+      if (File.Exists(filePath))
+      {
+        _editor.State = File.ReadAllText(filePath);
+      }
+      else
+      {
+        throw new FileNotFoundException("Файл не найден", filePath);
+      }
+    }
   }
 }
